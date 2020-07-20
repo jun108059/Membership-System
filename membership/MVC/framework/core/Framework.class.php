32f21805d75484c5631c0008884db54ade52a9f6
YoungJun Park <youngjun108059@gmail.com>
2020-07-20 15:33:23 +0900

    [Add] MVC 패턴으로 Refactoring 작업 시작
    MVC 패턴으로 Refactoring 작업 시작

---
+++ membership/MVC/framework/core/Framework.class.php
@@ -0,0 +1,24 @@
+<?php
+
+class Framework {
+
+    public static function run() {
+        self::init();
+        self::autoload();
+        self::dispatch();
+
+    }
+    private static function init() {
+
+    }
+
+
+    private static function autoload() {
+
+    }
+
+
+    private static function dispatch() {
+
+    }
+}
