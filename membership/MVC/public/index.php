33cc890fafb747991b2c9eae90d482ff6ae36ce1
YoungJun Park <youngjun108059@gmail.com>
2020-07-21 11:11:33 +0900

    [Add] FrontController 추가 - index.php
    FrontController 추가 - index.php

---
+++ membership/MVC/public/index.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Front Controller
+ */
+
+// echo 'Hello from the public folder!"'.$_SERVER['QUERY_STRING'].'"';
+
+/**
+ * Routing
+ */
+require_once '../Core/Router.php';
+
+$router = new Router();
+// echo get_class($router);
+
+// Add the routes
+$router->add('',['controller' => 'Home', 'action' => 'index']);
+$router->add('posts',['controller' => 'Posts', 'action' => 'index']);
+$router->add('posts/new',['controller' => 'Posts', 'action' => 'new']);
+
+// Display the routing table
+//echo '<pre>';
+//var_dump($router->getRoutes());
+//echo '</pre>';
+
+// Match the requested route
+$url = $_SERVER['QUERY_STRING'];
+
+if ($router->match($url)) {
+    echo '<pre>';
+    var_dump($router->getRoutes());
+    echo '</pre>';
+}else {
+    echo "No route found for URL '$url'";
+}
+
+
