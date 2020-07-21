5e7b6db42c442146cbe49b1a87c2b217b13c1677
YoungJun Park <youngjun108059@gmail.com>
2020-07-21 13:17:46 +0900

    [Update] Router 정규 표현식 preg_match() 적용
    reg_exp 정의

--- membership/MVC/Core/Router.php
+++ membership/MVC/Core/Router.php
@@ -25,6 +25,7 @@ class Router {
      * @return boolean 매칭 - true, 아니면 - false
      */
     public function match($url) {
+        /*
         foreach ($this->routes as $route => $params) {
             if ($url == $route) {
                 $this->params = $params;
@@ -32,6 +33,26 @@ class Router {
             }
         }
         return false;
+        */
+
+        // Match to the fixed URL format /controller/action
+        $reg_exp = "/^(?P<controller>[a-z-]+)\/(?P<action>[a-z-]+)$/";
+
+        if (preg_match($reg_exp, $url, $matches)) {
+            //Get named capture group values
+            $params = [];
+
+            foreach($matches as $key => $match) {
+                if (is_string($key)) {
+                    $params[$key] = $match;
+                }
+            }
+
+            $this->params = $params;
+            return true;
+        }
+
+//        return false;
     }
 
     // Get Routes
