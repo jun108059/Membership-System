fea35c3dce92218cf95be649d8cdfa2cd969c5f5
YoungJun Park <youngjun108059@gmail.com>
2020-07-22 10:54:31 +0900

    [Update] __call magic method + Action Filter 추가
    controller 안에서 check 로직 구현하기 위한 action filter

--- membership/MVC/App/Controllers/Home.php
+++ membership/MVC/App/Controllers/Home.php
@@ -2,15 +2,44 @@
 
 namespace App\Controllers;
 
-class Home
+class Home extends \Core\Controller
 {
+    // 메인 코어 Controller 를 상속 받음
+
+    /**
+     * Before filter
+     * @return void
+     */
+    protected function before()
+    {
+        echo "(before) ";
+        // return false;
+        // false 를 리턴 하면 메소드 실행 안 됨!
+        // 활용 하면 login 기능 구현 쉬움
+        /* 이런 식으로
+        if ( ! isset($_SESSION["user_id"])) {
+            return false;
+        }
+        */
+    }
+
+    /**
+     * After filter
+     * @return void
+     */
+    protected function after()
+    {
+        echo " (after)";
+    }
 
     /**
      * Show the index page
      * @return void
      */
-    public function index()
+    public function indexAction()
     {
         echo 'Hello from the index action in the Home controller!';
     }
+
+
 }
--- membership/MVC/App/Controllers/Posts.php
+++ membership/MVC/App/Controllers/Posts.php
@@ -10,7 +10,7 @@ class Posts extends \Core\Controller
      * 검색 기능
      * @return void
      */
-    public function index()
+    public function indexAction()
     {
         echo 'Hello from the index action in the Posts controller!';
         echo '<p>Query string parameters: <pre>' .
@@ -22,7 +22,7 @@ class Posts extends \Core\Controller
      * 새로운 게시글 작성 기능
      * @return void
      */
-    public function addNew()
+    public function addNewAction()
     {
         echo 'Hello from the addNew action in the Posts controller!';
     }
@@ -32,7 +32,7 @@ class Posts extends \Core\Controller
      *
      * @return void
      */
-    public function edit()
+    public function editAction()
     {
         echo 'Hello from the edit action in the Posts controller!';
         echo '<p>Route parameters: <pre>' .
