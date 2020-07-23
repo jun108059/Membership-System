c69b4eb0d221e8366038473d44a407d5d6d19c81
YoungJun Park <youngjun108059@gmail.com>
2020-07-23 09:32:43 +0900

    [Add] 🛢 Core 추상화 된 Model 선언 & App 상속
    Core에 Model.php 추가 - 연결을 위한 getDB( ) 메소드 추가

--- membership/MVC/App/Models/Post.php
+++ membership/MVC/App/Models/Post.php
@@ -3,11 +3,12 @@
 namespace App\Models;
 
 use PDO;
+use PDOException;
 
 /**
  * Post model
  */
-class Post
+class Post extends \Core\Model
 {
 
     /**
@@ -17,14 +18,16 @@ class Post
      */
     public static function getAll()
     {
-        $host = 'localhost';
-        $dbname = 'mvc';
-        $username = 'root';
-        $password = 'secret';
+//        $host = 'localhost';
+//        $dbname = 'membership';
+//        $username = 'root';
+//        $password = '1234';
 
         try {
-            $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8",
-                $username, $password);
+            // $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8",
+            //    $username, $password);
+            // 추상화 Core Model 클래스 - getDB() 호출
+            $db = static::getDB();
 
             $stmt = $db->query('SELECT id, title, content FROM posts
                                 ORDER BY created_at');
---
+++ membership/MVC/Core/Model.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Core;
+
+use PDO;
+use PDOException;
+
+/**
+ * Base model
+ *
+ * PHP version 7.4
+ */
+abstract class Model
+{
+
+    /**
+     * Get the PDO database connection
+     *
+     * @return mixed
+     */
+    protected static function getDB()
+    {
+        static $db = null;
+
+        if ($db === null) {
+            $host = 'localhost';
+            $dbname = 'membership';
+            $username = 'root';
+            $password = '1234';
+    
+            try {
+                // DB 연결 코드
+                $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", 
+                              $username, $password);
+
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+            }
+        }
+
+        return $db;
+    }
+}
