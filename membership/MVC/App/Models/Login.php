7583eaa09bd67daee5845a5495027abeba3c4785
YoungJun Park <youngjun108059@gmail.com>
2020-07-29 18:09:04 +0900

    [Refact]♻Login Model 불필요 코드 삭제&최적화
    getPassword 메소드 삭제

--- membership/MVC/App/Models/Login.php
+++ membership/MVC/App/Models/Login.php
@@ -10,44 +10,21 @@ use PDOException;
  */
 class Login extends \Core\Model
 {
-    public static function getPassword($mem_id)
+
+    public static function getUserData($userId)
     {
-        try {
-            // 추상화 Core Model 클래스 - getDB() 호출
-            // DB 연결
-            $db = static::getDB();
-            // 쿼리를 담은 PDOStatement 객체 생성 - return PDOStatement 객체
-            $stmt = $db->prepare("SELECT mem_password FROM user WHERE mem_user_id = :user_id");
+        // DB 연결 - Abstract Core Model 클래스 - getDB() 호출
+        $db = static::getDB();
+        // 쿼리를 담은 PDOStatement 객체 생성 - return PDOStatement 객체
+        $stmt = $db->prepare("SELECT * FROM user WHERE mem_user_id = :user_id");
 
-            // PDOStatement 객체가 가진 쿼리의 parameter 에 변수 값을 바인드
-            $stmt -> bindValue(":user_id", $mem_id);
+        // PDOStatement 객체가 가진 쿼리의 parameter 에 변수 값을 바인드
+        $stmt->bindValue(":user_id", $userId, PDO::PARAM_STR);
 
-            // PDOStatement 객체가 가진 쿼리를 실행
-            $stmt -> execute();
+        // PDOStatement 객체가 가진 쿼리를 실행
+        $stmt->execute();
 
-            // PDOStatement 객체가 실행한 쿼리의 결과값 가져 오기
-            //            echo "<pre>";
-//            print_r($row);
-//            echo "</pre>";
-            return $stmt -> fetch();
-        } catch (PDOException $e) {
-            echo $e->getMessage();
-        }
-    }
-
-    public static function getIdPassword()
-    {
-        try {
-            // 추상화 Core Model 클래스 - getDB() 호출
-            // DB 연결
-            $db = static::getDB();
-
-            $stmt = $db->query('SELECT mem_user_id, mem_password FROM user');
-            return $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-        } catch (PDOException $e) {
-            echo $e->getMessage();
-        }
+        return $stmt->fetch();
     }
 
     public static function LoginCheck()
