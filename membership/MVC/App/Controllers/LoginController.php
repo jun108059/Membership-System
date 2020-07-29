f4a55f7acc1e2df70ca7000ed90b2f1ad1452796
YoungJun Park <youngjun108059@gmail.com>
2020-07-29 18:05:41 +0900

    [Update]🕹로그인 page Session 값 할당 후 Rendering
    Session에 값 할당

--- membership/MVC/App/Controllers/LoginController.php
+++ membership/MVC/App/Controllers/LoginController.php
@@ -4,37 +4,48 @@ namespace App\Controllers;
 
 use \Core\View;
 use App\Models\Login;
-use SessionService;
+use App\Service\SessionManager;
 
 class LoginController extends \Core\Controller
 {
+
+    /**
+     * Show the index page
+     * @return void
+     */
+    public function indexAction()
+    {
+        View::render('Login/index.php', []);
+    }
+
+    /**
+     * Login 검사
+     */
     public function loginCheckAction()
     {
-        // 모델 에서 데이터 꺼내 오기
-        $user = Login::getIdPassword();
+        //        ★ View 에서 체크
+//        if (empty($user_id) || empty($user_pw)) { // empty 로 빈값 체크
+//            echo '<script> alert(" ❓아이디 또는 패스워드 입력하세요❓"); history.back(); </script>';
+//        }
 
         // 각 변수에 ID, PW 저장
         $user_id = $_POST['user_id'];
         $user_pw = $_POST['user_password'];
+        // 모델 에서 데이터 꺼내 오기
+        $user = Login::getUserData($user_id);
+        $pw_check = $user['mem_password'];
+        $user_log = $user['mem_log_dt'];
 
-        if (empty($user_id) || empty($user_pw)) { // empty 로 빈값 체크
-            echo '<script> alert(" ❓아이디 또는 패스워드 입력하세요❓"); history.back(); </script>';
-        } else {
-            // pw_check 변수에 Login Model 저장 password 받아 오기
-            $pw_check = Login::getPassword($user_id);
+        //만약 password 와 hash_pw 가 같다면 세션 실행
+        if (password_verify($user_pw, $pw_check)) {
 
-            //만약 password 와 hash_pw 가 같다면 세션 값을 저장
-            //알림창 띄운후 Home rendering
-            if (password_verify($user_pw, $pw_check['mem_password'])) {
-                $session_manager = new SessionService();
-//                session_start();
-                $_SESSION['mem_user_id'] = $user['mem_user_id'];
-                $_SESSION['mem_password'] = $user['mem_password'];
-
-                echo "<script>alert('✔ 로그인 되었습니다 ✔'); location.href='/home/index';</script>";
-            } else { // 비밀번호가 같지 않다면 알림창을 띄우고 전 페이지로 돌아갑니다
-                echo "<script>alert('❗ 아이디 또는 비밀번호를 확인하세요 ❗'); history.back();</script>";
-            }
+            $session_manager = new SessionManager();
+            $session_manager->setSessionValue('userID', $user_id);
+            $session_manager->setSessionValue('userLog', $user_log);
+            $session_manager->update_active_time();
+            View::render('Login/loginOK.html', []);
+        } else { // 비밀번호가 같지 않다면 알림창을 띄우고 전 페이지로 돌아갑니다
+            echo "<script>alert('❗ 아이디 또는 비밀번호를 확인하세요 ❗'); history.back();</script>";
         }
         /** Render 를 Home 으로 해줘야 되면 쓰기
          * 내 생각엔 그냥 location 으로 url 줘도 될 듯 (검사 했기 때문에)
