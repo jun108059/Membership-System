8873d12dd453566488a462a4c1f5930d016985cc
YoungJun Park <youngjun108059@gmail.com>
2020-07-29 18:11:42 +0900

    [Update]✨Home 메인 page 로그아웃 기능 추가
    Home 메인 page 로그아웃 기능 추가

--- membership/MVC/App/Views/Home/index.php
+++ membership/MVC/App/Views/Home/index.php
@@ -1,3 +1,7 @@
+<?php
+print_r($_SESSION);
+?>
+
 <!DOCTYPE html>
 <html lang="ko">
 <head>
@@ -5,7 +9,20 @@
     <title>Home</title>
 </head>
 <body>
-<h1>Welcome</h1>
-<p>안녕하세요! 멤버쉽 서비스입니다.</p>
+<h1>멤버쉽 시스템 Home</h1>
+<!--<p>안녕하세요! <span id="name"></span> 님 멤버쉽 서비스입니다.</p>-->
+<p>hi ! <?php echo "<h2> {$_SESSION['userID']} 님 환영합니다😄</h2>"?></p>
+<a href="/Login/logout"><input type="button" value="로그아웃" /></a>
+<a href="/Home/infoModify"><input type="button" value="로그아웃" /></a>
+<!--<script type="text/javascript" src="//code.jquery.com/jquery.min.js"></script>-->
+<!--<script>-->
+<!--    $.ajax({-->
+<!--        url: "/Home/myName",-->
+<!--        type: "GET",-->
+<!--    }).done(function(data) {-->
+<!--        $('#name').text(data);-->
+<!--    });-->
+<!--</script>-->
 </body>
 </html>
+
--- membership/MVC/App/Views/Home/myname.php
+++ membership/MVC/App/Views/Home/myname.php
@@ -1,2 +1,16 @@
 <?php
-echo('YoungJun Park');
+
+//use App\Service\SessionManager;
+////use Core\View;
+//
+//$session_manager = new SessionManager();
+////        print_r($_SESSION);
+////유효한 접근이 아니거나 로그인 유효시간이 지나면 로그인 page 로 이동
+//if (!$session_manager->isValidAccess() || $session_manager->isLoginExpired()) {
+//    $session_manager->destroy_session();
+//} // 로그인 유효 시간 갱신
+//else{
+//    $session_manager->update_active_time();
+//}
+
+echo $_SESSION['mem_user_id'];
