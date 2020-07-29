270fbd543b46b192be80e7544b212055c84ccf20
YoungJun Park <youngjun108059@gmail.com>
2020-07-29 18:04:18 +0900

    [Update]🕹메인 Page Session 확인 로직 추가
    세션 유효 시간, 접근 확인 후 Rendering

--- membership/MVC/App/Controllers/HomeController.php
+++ membership/MVC/App/Controllers/HomeController.php
@@ -3,26 +3,31 @@
 namespace App\Controllers;
 
 use \Core\View;
+use App\Service\SessionManager;
 
 class HomeController extends \Core\Controller
 {
-    // 메인 코어 Controller 를 상속 받음
-
     /**
-     * Show the index page
+     * 메인 page 로 Rendering
      * @return void
      */
     public function indexAction()
     {
-        // index action in the HomeController
-        // View 렌더링 추가!
-
-        View::render('Home/index.html', []);
-
-//        View::renderTemplate('Home/index.html', [
-//            'name'    => 'YoungJun',
-//            'colours' => ['red', 'green', 'blue']
-//        ]);
+        $session_manager = new SessionManager();
+        print_r($_SESSION);
+//        exit();
+        //유효한 접근이 아니거나 로그인 유효시간이 지나면 로그인 page 로 이동
+        if (!$session_manager->isValidAccess() || $session_manager->isLoginExpired()) {
+            $session_manager->destroy_session();
+            echo '<script> alert("🔴잘못된 접근입니다. 로그인 후 이용해주세요!🔴"); </script>';
+            View::render('Login/index.php', []);
+        } // 로그인 유효 시간 갱신
+        else{
+            $session_manager->update_active_time();
+            View::render('Home/index.php', [
+                'session' => $session_manager
+            ]);
+        }
     }
 
     public function myNameAction()
@@ -30,5 +35,20 @@ class HomeController extends \Core\Controller
         View::render('Home/myname.php', []);
     }
 
+    public function infoModifyAction() {
+        $session_manager = new SessionManager();
+        print_r($_SESSION);
+        if (!$session_manager->isValidAccess() || $session_manager->isLoginExpired()) {
+            $session_manager->destroy_session();
+            echo '<script> alert("🔴잘못된 접근입니다. 로그인 후 이용해주세요!🔴"); </script>';
+            View::render('Login/index.php', []);
+        } // 로그인 유효 시간 갱신
+        else{
+            $session_manager->update_active_time();
+            View::render('Home/infoModify.php', [
+                'session' => $session_manager
+            ]);
+        }
+    }
 
 }
