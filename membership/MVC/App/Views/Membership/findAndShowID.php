b7d8a2de772700c929399d7d33de525601fa83d2
YoungJun Park <youngjun108059@gmail.com>
2020-07-30 17:48:55 +0900

    [Update]✨ID 찾기 페이지 구현
    고객 ID 받아와서 출력

---
+++ membership/MVC/App/Views/Membership/findAndShowID.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <title>아이디 찾기</title>
+</head>
+<body>
+<h1>아이디 찾기 완료</h1>
+<p>고객님의 아이디는 <?php echo $userId?> 입니다.</p>
+<br><br>
+<a href="/"><input type="button" value="로그인 페이지" /></a>
+<a href="/Membership/findMyInfo"><input type="button" value="비밀번호 찾기" /></a>
+
