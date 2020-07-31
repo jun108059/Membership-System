2392e8edf1645a321f45ee0168c63643afbf4134
YoungJun Park <youngjun108059@gmail.com>
2020-07-31 16:49:37 +0900

    [Update]✨아이디 비밀번호 찾기 구현 완료
    비밀번호 찾기 이메일 본인 인증 구현

--- membership/MVC/App/Views/Membership/findMyInfo.php
+++ membership/MVC/App/Views/Membership/findMyInfo.php
@@ -10,8 +10,13 @@
 </head>
 <body>
 <div class="find">
-    <form id="findID" action="/Membership/findId" method="post" >
+    <form action="/Membership/findId" method="post" >
         <h1>회원계정 찾기🔎</h1>
+
+        <input type="hidden" name="cert_finish" value="y">
+        <input type="hidden" id="can_cert_time" value="n">
+        <input type="hidden" id="correct_cert_num" value="">
+
         <br>
         <p><a href="/">홈으로</a></p>
         <br>
@@ -38,19 +43,34 @@
     </form>
 </div>
 <div class="find">
-    <form method="post" action="Membership/findPw">
+    <form action="passwordChange" method="post" >
         <fieldset>
             <legend>비밀번호 찾기</legend>
             <table>
                 <tr>
                     <td>아이디</td>
-                    <td><input type="text" size="35" name="user_id" placeholder="아이디"></td>
+                    <td><input type="text" id="user_id" name="user_id" size="35" placeholder="아이디"></td>
                 </tr>
             </table>
-            <input type="submit" value="비밀번호 찾기" />
         </fieldset>
+        <a href="#" id="send_email_btn">본인 인증 후 비밀번호 재설정</a>
+
+        <div id="certify_num_area" style="display: none;">
+            <h1>인증번호를 입력해주세요!</h1>
+            <p>
+                <strong>인증번호</strong>
+                <input type="text" name="cert_num" id="cert_num" size="20" placeholder="6자리 숫자" maxlength="6"/>
+                <input type="submit" id="cert_num_btn" value="인증하기"  />
+                <!--            <a href="/Membership/signUp" id="cert_num_btn">인증하기</a>-->
+            </p>
+            <div id="timeView">인증 시간 : </div>
+            <p id="result"></p>
+        </div>
     </form>
 </div>
+
+
+
 
 <script type="text/javascript" src="https://code.jquery.com/jquery.min.js"></script>
 <script>
@@ -61,32 +81,42 @@
                 name = $('#name').val();
             if (name === '') {
                 alert('이름을 입력해주세요');
-            }else if (email === '') {
-                alert('이메일 주소를 입력해주세요');
+                return false;
             }
+            else if (email === '') {
+                alert('이메일 주소를 입력해주세요');
+                return false;
+            }
+            else {
+                $('form').submit();
+            }
+        });
 
+        $('#send_email_btn').click(function () {
+            var userId = $('#user_id').val();
+            if (userId === '') {
+                alert('아이디를 입력해주세요!');
+                return false;
+            }
             $.ajax({
-                url: "/Membership/sendMail",
+                url: "/Membership/emailForFindPw",
                 method: 'POST',
-                data: {email: email, emAddress: emAddress},
+                data: {user_id: userId},
                 dataType: "json",
                 async: false
             }).done(function (data) {
-                // alert(data.result);
                 if (data.result === 'success') {
                     alert('이메일이 전송되었습니다.\n3분 내로 본인인증을 완료해주세요.');
                     timer();
                     $('#can_cert_time').val('y'); // 이메일 인증 유효 시간 내
                     $('#correct_cert_num').val(data.cert_num);
                     $('#certify_num_area').show();
-                    $("#email").attr("readonly", true);
-                    $("#emadress").attr("disabled", true);
+                    $("#user_id").attr("readonly", true);
                 } else if (data.alert !== ''){
                     alert(data.alert);
                 }else{
                     alert("오류가 발생했습니다.");
                 }
-
                 return false;
             });
 
@@ -101,13 +131,34 @@
                     return false;
                 }
                 alert("🎉본인인증이 완료되었습니다!");
-                $('#emadress').attr("disabled", false);
                 $('form').submit();
             });
         });
     });
+
+    function timer() {
+        var time = 180; // 기준 시간
+        var min = '';  // 분
+        var sec = ''; // 초
+
+        //setInterval(함수, 시간) : 주기적 실행
+        var x = setInterval(function () {
+            //parseInt() : 정수 반환
+            min = parseInt(time / 60); // 몫
+            sec = time % 60; // 나머지
+
+            $('#timeView').html("남은 인증 시간 : " + min + "분 " + sec + "초");
+            time--;
+
+            // 타임 아웃
+            if (time < 0) {
+                clearInterval(x); // setInterval 종료
+                $('#timeView').html("인증 시간이 초과되었습니다");
+                $('#can_cert_time').val('n'); // 이메일 인증 유효시간 아닐때
+            }
+        }, 1000);
+    }
 </script>
 
 </body>
 </html>
-
