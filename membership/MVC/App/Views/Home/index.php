3d03c0023df99e2f1080afb4bb7eb5ad4ea7056c
YoungJun Park <youngjun108059@gmail.com>
2020-07-31 09:02:38 +0900

    [Update]✨개인정보 수정 페이지 View 제작
    개인정보 수정 페이지 View

--- membership/MVC/App/Views/Home/index.php
+++ membership/MVC/App/Views/Home/index.php
@@ -1,7 +1,3 @@
-<?php
-print_r($_SESSION);
-?>
-
 <!DOCTYPE html>
 <html lang="ko">
 <head>
@@ -12,8 +8,9 @@ print_r($_SESSION);
 <h1>멤버쉽 시스템 Home</h1>
 <!--<p>안녕하세요! <span id="name"></span> 님 멤버쉽 서비스입니다.</p>-->
 <p>hi ! <?php echo "<h2> {$_SESSION['userID']} 님 환영합니다😄</h2>"?></p>
+<br><br>
+<a href="/Home/infoModify"><input type="button" value="개인정보수정" /></a>
 <a href="/Login/logout"><input type="button" value="로그아웃" /></a>
-<a href="/Home/infoModify"><input type="button" value="로그아웃" /></a>
 <!--<script type="text/javascript" src="//code.jquery.com/jquery.min.js"></script>-->
 <!--<script>-->
 <!--    $.ajax({-->
