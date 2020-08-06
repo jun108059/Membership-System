4e01f7ed4d3a43c8bdccb54a9e17d9593cde46f5
YoungJun Park <youngjun108059@gmail.com>
2020-08-06 19:07:59 +0900

    [Refact]♻Nginx+PHP7 환경설정 conf 파일 이동
    nginx 폴더로 이전

---
+++ nginx/html/member/login_ok.php
@@ -0,0 +1,27 @@
+<meta charset="utf-8"/>
+<?php
+    include "../db.php";
+    include "../password.php";
+
+    // POST로 받아온 아이다와 비밀번호가 비었다면 알림창을 띄우고 전 페이지로 돌아감
+    if ($_POST["mem_user_id"] == "" || $_POST["mem_password"] == "") {
+        echo '<script> alert("아이디와 패스워드 입력하세요"); history.back(); </script>';
+    } else {
+
+        // password 변수에 POST로 받아온 값을 저장하고 sql문으로 POST로 받아온 아이디 값을 찾자
+        $password = $_POST['mem_password'];
+        $sql = mq("select * from user where mem_user_id='" . $_POST['mem_user_id'] . "'");
+        $user = $sql->fetch_array();
+        $hash_pw = $user['mem_password']; //$hash_pw에 post로 받아온 아이디열의 비밀번호를 저장합니다.
+
+    if (password_verify($password, $hash_pw)) //만약 password변수와 hash_pw변수가 같다면 세션값을 저장하고 알림창을 띄운후 main.php파일로 넘어갑니다.
+    {
+        $_SESSION['mem_user_id'] = $user["mem_user_id"];
+        $_SESSION['mem_password'] = $user["mem_password"];
+
+        echo "<script>alert('로그인되었습니다.'); location.href='/page/index.php';</script>";
+    } else { // 비밀번호가 같지 않다면 알림창을 띄우고 전 페이지로 돌아갑니다
+        echo "<script>alert('아이디 또는 비밀번호를 확인하세요.'); history.back();</script>";
+    }
+}
+?>
---
+++ nginx/html/member/sendmail.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8"/>
+    <title>Hi 나는 샌드메일이야</title>
+    <link rel="stylesheet" type="text/css" href="/css/common.css"/>
+</head>
+</html>
