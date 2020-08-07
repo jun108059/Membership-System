38c8667f71710935f9e1c33f6938d5e6b9621f4a
YoungJun Park <youngjun108059@gmail.com>
2020-08-07 10:41:24 +0900

    [Update]🛢로그인 Log Table 삽입 로직 수정
    🛢로그인 Log Table 삽입 로직 수정

--- MVC/App/Models/Login.php
+++ MVC/App/Models/Login.php
@@ -49,7 +49,7 @@ class Login extends \Core\Model
     }
 
     /**
-     * 로그인 기록 Update
+     * 최근 로그인 시간 Update
      * @param $userData
      * @return bool
      */
@@ -68,39 +68,21 @@ class Login extends \Core\Model
             'dateTime'  => $userData['mem_log_dt'],
             'id'        => $userData['mem_user_id']
         ];
-        //todo 트렌젝션 처리
-
         $sql = "UPDATE user SET mem_log_dt = :dateTime WHERE mem_user_id = :id";
         $stmt = $db->prepare($sql);
         // binding 값 넘겨서 실행
-        $stmt->execute($bindArray);
-
-        // user 테이블에서 가져온 값 로그 테이블에 저장하기 위해 bind
-        $bindArray = [
-            'mem_idx'       => $userData['mem_idx'],
-            'id'            => $userData['mem_user_id'],
-            'log_type'      => 'IN',
-            'date_time'     => $userData['mem_log_dt'],
-        ];
-
-        $sql2 = "INSERT INTO login_logout_log SET
-                     mem_idx        = :mem_idx,
-                     id             = :id,
-                     log_type       = :log_type,
-                     date_time      = :date_time
-        ";
-        $stmt = $db->prepare($sql2);
         $stmt->execute($bindArray);
         return true;
         // 에러 처리 필요
     }
 
     /**
-     * 로그아웃 Log 남기기
+     * Log Table INSERT
      * @param $userData
+     * @param $logValue
      * @return bool
      */
-    public static function updateLogOutLog($userData)
+    public static function logTableInsert($userData, $logValue)
     {
         // $user = 배열 && !Null 검사
         if (empty($userData) || !is_array($userData)) {
@@ -111,31 +93,21 @@ class Login extends \Core\Model
         // DB 연결
         $db = static::getDB();
 
-        $bindArray = [
-            'dateTime'  => $userData['mem_log_dt'],
-            'id'        => $userData['mem_user_id']
-        ];
-
-        $sql = "UPDATE user SET mem_log_dt = :dateTime WHERE mem_user_id = :id";
-        $stmt = $db->prepare($sql);
-        // binding 값 넘겨서 실행
-        $stmt->execute($bindArray);
-
         // user 테이블에서 가져온 값 로그 테이블에 저장하기 위해 bind
         $bindArray = [
             'mem_idx'       => $userData['mem_idx'],
             'id'            => $userData['mem_user_id'],
-            'log_type'      => 'OUT',
+            'log_type'      => $logValue,
             'date_time'     => $userData['mem_log_dt'],
         ];
 
-        $sql2 = "INSERT INTO login_logout_log SET
+        $sql = "INSERT INTO login_logout_log SET
                      mem_idx        = :mem_idx,
                      id             = :id,
                      log_type       = :log_type,
                      date_time      = :date_time
         ";
-        $stmt = $db->prepare($sql2);
+        $stmt = $db->prepare($sql);
         $stmt->execute($bindArray);
         return true;
         // 에러 처리 필요
