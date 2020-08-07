65dc039c67501c959f85f2c0d57ebc251b4c5337
YoungJun Park <youngjun108059@gmail.com>
2020-08-07 18:31:11 +0900

    [Update]✨관리자 페이지 휴면계정 버튼 추가

--- MVC/App/Views/Admin/index.php
+++ MVC/App/Views/Admin/index.php
@@ -23,21 +23,27 @@ include('head.php');
             <tr>
                 <th> 사용자 정보 검색</th>
                 <th> 휴면 계정 알림</th>
+                <th> 휴면 계정 전환</th>
+                <th> 휴면 계정 파기</th>
                 <th> 미니 CRM</th>
             </tr>
             </thead>
 
             <tbody>
-                <tr>
-                    <td><a class="btn btn-primary" href="/Admin/allUserInfo/1"><span class="glyphicon glyphicon-menu-hamburger"></span> 사용자보기</a></td>
-                    <td><a class="btn btn-danger" href="/Dormant/dormantNoticeMail" onclick="return confirm('휴면 전환 알림 메일을 보낼까요?')">
-                            <span class="glyphicon glyphicon-send"></span> 메일 전송</a></td>
-                    <td><a class="btn btn-warning" href="" onclick="return confirm('⏰곧 구현하겠습니당😄')">
-                            <span class="glyphicon glyphicon-signal"></span> 제작 중</a></td>
-                </tr>
+            <tr>
+                <td><a class="btn btn-primary" href="/Admin/allUserInfo/1"><span class="glyphicon glyphicon-menu-hamburger"></span> 사용자보기</a></td>
+                <td><a class="btn btn-info" href="/Dormant/dormantNoticeMail" onclick="return confirm('휴면 전환 알림 메일을 보낼까요?')">
+                        <span class="glyphicon glyphicon-envelope"></span> 메일 전송</a></td>
+                <td><a class="btn btn-success" href="/Dormant/turnIntoDormant" onclick="return confirm('30일 미접속자 휴면 계정으로 전환합니다.')">
+                        <span class="glyphicon glyphicon-send"></span> 계정 전환</a></td>
+                <td><a class="btn btn-danger" href="/Dormant/deleteDormant" onclick="return confirm('60일 미접속 휴면 계정 정보를 파기합니다.')">
+                        <span class="glyphicon glyphicon-trash"></span> 정보 파기</a></td>
+                <td><a class="btn btn-warning" href="" onclick="return confirm('⏰곧 구현하겠습니당😄')">
+                        <span class="glyphicon glyphicon-signal"></span> 제작 중</a></td>
+            </tr>
             </tbody>
         </table>
     </div>
 
-    </body>
-    </html>
+</body>
+</html>
