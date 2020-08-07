75cca32fbc4c9e869c33f958c0d1df1c20e77217
YoungJun Park <youngjun108059@gmail.com>
2020-08-07 10:38:53 +0900

    [Update]🕹로그인 Error 핸들링, Log Table 갱신 로직 수정
    로그인 활동 시간 갱신 성공 -> Log Table 업데이트

--- MVC/App/Controllers/LoginController.php
+++ MVC/App/Controllers/LoginController.php
@@ -15,7 +15,7 @@ class LoginController extends \Core\Controller
      */
     public function indexAction()
     {
-        if (isset($_SESSION['userID'])) {
+        if (!empty($_SESSION['userID'])) {
             if ($_SESSION['userID'] === 'admin' && $_SESSION['userLevel'] === '1') {
                 View::render('Admin/loginOK.html', []);
             } else {
@@ -37,11 +37,22 @@ class LoginController extends \Core\Controller
 
         // View & Back-end 이중 체크
         if (empty($user_id) || empty($user_pw)) { // empty 로 빈값 체크
-            echo '<script> alert(" ❓아이디 또는 패스워드 입력하세요❓"); history.back(); </script>';
+            View::render('Error/errorPage.php', [
+                'alert' => "아이디 또는 패스워드 입력하세요❗",
+                'back' => "ture"
+            ]);
+            exit();
         }
 
         // 모델 에서 데이터 꺼내 오기
         $user = Login::getUserData($user_id);
+        if (empty($user)) {
+            View::render('Error/errorPage.php', [
+                'alert' => "존재하지 않는 User 입니다❗",
+                'back' => "ture"
+            ]);
+            exit();
+        }
         $pw_check = $user['mem_password'];
 //        $user_log = $user['mem_log_dt'];
         $status_check = $user['mem_status']; // 계정 상태 확인
@@ -49,20 +60,29 @@ class LoginController extends \Core\Controller
 
         //만약 password 와 hash_pw 가 같다면 세션 실행
         if (password_verify($user_pw, $pw_check)) {
-            session_start();
             if ($status_check === 'H') {
-                echo '<script> alert("고객님은 휴면계정입니다!🔒"); history.back(); </script>';
+                echo '<script> alert("고객님은 휴면계정입니다!🔒"); </script>';
                 $_SESSION["userID"] = $user_id;
                 $_SESSION["userEmail"] = $user['mem_email'];
-                // 휴면 계정이라면
-                // 세션 유지할 필요 없음
                 View::render('Login/dormant.php', [
                     'user_email' => $_SESSION["userEmail"]
                 ]);
                 exit();
             }
             $user['mem_log_dt'] = (new DateTime())->format('Y-m-d H:i:s');
-            Login::updateLogInDate($user); // todo 나누기/ 성공했을때로그처리
+            $logValue = "IN";
+            // 로그인 - 활동 시간 갱신
+            if(Login::updateLogInDate($user)) {
+                // 로그인 성공 시 Log Table 에 삽입
+                if(!(Login::logTableInsert($user, $logValue))){
+                    View::render('Error/errorPage.php', [
+                        'alert' => "오류가 발생했습니다. 로그인을 다시 시도해주세요.",
+                        'back' => "ture"
+                    ]);
+                    exit();
+                }
+            }
+
             $_SESSION["userID"] = $user_id;
             $_SESSION["userLog"] = $user['mem_log_dt'];
             $_SESSION["userLevel"] = $user['mem_level'];
@@ -87,9 +107,10 @@ class LoginController extends \Core\Controller
         $userId = $_SESSION['userID'];
         $user = Login::getUserData($userId);
         $user['mem_log_dt'] = (new DateTime())->format('Y-m-d H:i:s');
-        Login::updateLogOutLog($user);
+        $logValue = "OUT";
+        Login::logTableInsert($user, $logValue);
         session_destroy();
-        View::render('Login/logout.html', []);
+        View::render('Login/logout.html');
     }
 
 }
